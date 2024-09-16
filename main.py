e6cef048915d8c98072baf94f1c542c835f662f9
LeoGidev <leogimenez.dev@gmail.com>
2024-09-16 20:07:26 -0300

    se crea funcion que obtiene la hora

--- main.py
+++ main.py
@@ -5,3 +5,5 @@ import time
 
 # URL de la API para obtener la hora
 url = 'http://worldtimeapi.org/api/timezone/America/Buenos_aires'
+# Función para obtener la fecha y la hora de internet
+def obtener_hora_internet():
