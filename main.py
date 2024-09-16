4477d31a4cd04c17aca386a9ee75fa5507b41950
LeoGidev <leogimenez.dev@gmail.com>
2024-09-16 20:17:23 -0300

    se comentan librerías no usadas

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 import subprocess
-import os
-import time
+#import os
+#import time
 
 # URL de la API para obtener la hora
 url = 'http://worldtimeapi.org/api/timezone/America/Buenos_aires'
